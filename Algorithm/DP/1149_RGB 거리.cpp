c2190d7d3b56a8631ffc81bc59416c50ee12f3e8
KyuJeong <heo0414@naver.com>
2018-01-03 21:28:42 +0900

    DP 기본 젛뫄식

---
+++ Algorithm/DP/1149_RGB 거리.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+int R, G, B;
+int DP[3];
+int DP_nxt[3];
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> R >> G >> B;
+		DP_nxt[0] = min(DP[1], DP[2]) + R;
+		DP_nxt[1] = min(DP[0], DP[2]) + G;
+		DP_nxt[2] = min(DP[0], DP[1]) + B;
+
+		for (int j = 0; j < 3; j++)
+			DP[j] = DP_nxt[j];
+	}
+
+	cout << *min_element(DP, DP + 3);
+
+	return 0;
+}
