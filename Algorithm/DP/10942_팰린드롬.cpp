4a34a1274a562a4ad4d073688fdb63550f930812
KyuJeong <heo0414@naver.com>
2018-01-04 02:56:15 +0900

    DP 최적화

---
+++ Algorithm/DP/10942_팰린드롬.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define MAX 2001
+
+int DP[MAX][MAX];
+vector <int> arr(MAX);
+int N, M, S, E;
+ 
+int main()
+{
+	scanf("%d", &N);
+
+	for (int i = 1; i <= N; i++)
+		scanf("%d", &arr[i]);
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 1; j <= N - i; j++)
+		{
+			if (i == 0)
+				DP[j][j] = 1;
+			else if (i == 1)
+			{
+				if (arr[j] == arr[j + 1])
+					DP[j][j + 1] = 1;
+			}
+			else
+			{
+				if (arr[j] == arr[j + i] && DP[j + 1][j + i - 1] == 1)
+					DP[j][j + i] = 1;
+			}
+		}
+	}
+
+	scanf("%d", &M);
+
+	while (M--)
+	{
+		scanf("%d %d", &S, &E);
+		printf("%d\n", DP[S][E]);
+	}
+
+	return 0;
+}
