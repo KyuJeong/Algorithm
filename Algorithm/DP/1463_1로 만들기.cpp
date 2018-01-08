efb50ede1269cc9542b9117b270c848eb0fb1467
KyuJeong <heo0414@naver.com>
2018-01-08 12:57:26 +0900

    DP 점화식 기초

--- Algorithm/DP/1463_1로 만들기.cpp
+++ Algorithm/DP/1463_1로 만들기.cpp
@@ -1,72 +1,25 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-int cur[100000];
-int nxt[100000];
+int DP[1000001];
+int N;
 
 int main()
 {
 	std::ios::sync_with_stdio(false);
 
-	int N;
-
 	cin >> N;
-	cur[0] = N;
-	int cnt = 1;
-	int cur_cnt = 0;
-	int ans = 0;
 
-	for (int i = 0; i < N; i++)
+	for (int i = 2; i <= N; i++)
 	{
-		for (int j = 0; j < cnt; j++)
-		{
-			if (cur[j] % 3 == 0)
-			{
-				nxt[cur_cnt] = cur[j] / 3;
-				if (nxt[cur_cnt] == 1)
-				{
-					ans = i;
-					break;
-				}
-				cur_cnt++;
-
-			}
-
-			else
-			{
-				if (cur[j] % 2 == 0)
-				{
-					nxt[cur_cnt] = cur[j] / 2;
-					if (nxt[cur_cnt] == 1)
-					{
-						ans = i;
-						break;
-					}
-					cur_cnt++;
-
-				}
-				nxt[cur_cnt] = cur[j] - 1;
-				if (nxt[cur_cnt] == 1)
-				{
-					ans = i;
-					break;
-				}
-				cur_cnt++;
-			}
-		}
-
-		if (ans != 0)
-			break;
-		for (int j = 0; j < cur_cnt; j++)
-		{
-			cur[j] = nxt[j];
-		}
-
-		cnt = cur_cnt;
-		cur_cnt = 0;
+		DP[i] = DP[i - 1] + 1;
+		if (i % 2 == 0)
+			DP[i] = min(DP[i / 2], DP[i - 1]) + 1;
+		if (i % 3 == 0)
+			DP[i] = min(DP[i / 3], DP[i - 1]) + 1;
 	}
 
-	cout << ans + 1;
-
+	cout << DP[N];
 	return 0;
 }
