a3a170294703d1d8d3c2769cd84f47e7d6365965
KyuJeong <heo0414@naver.com>
2018-02-22 17:33:38 +0900

    지도에서 로봇 청소기 작동(구현)

---
+++ Algorithm/SW test/14503_로봇 청소기.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+using namespace std;
+
+#define MOD 4
+
+int N, M;
+int r, c, d;
+int G[50][50];
+int my[] = { -1,0,1,0 };
+int mx[] = { 0,1,0,-1 };
+int ans;
+
+void Clean()
+{	
+	int cy = r;
+	int cx = c;
+	while (1)
+	{
+		bool clean_flag = false;
+		G[cy][cx] = -1;	// ÇöÀç À§Ä¡ Ã»¼Ò
+		ans++;
+		int num = 0;
+		while(1)
+		{
+			d += 3;	// ¿ÞÂÊºÎÅÍ Å½»ö ÁøÇà
+			d %= MOD;
+			int ny = cy + my[d];
+			int nx = cx + mx[d];
+			num++;
+
+			if (G[ny][nx] == 0)	// 2-1
+			{
+				cy = ny;
+				cx = nx;
+				clean_flag = true;
+				break;
+			}
+			else // 2-2
+			{
+				if (num != 4)
+					continue;
+				else
+				{
+					cy -= my[d];
+					cx -= mx[d];
+					if (G[cy][cx] == 1) // 2-4 (ÀÛµ¿ ¸ØÃã)
+					{
+						clean_flag = false;
+						break;
+					}
+					num = 0;
+					continue; // 2-3
+				}
+			}
+		}
+		if (!clean_flag)
+			break;
+	}
+}
+
+int main()
+{
+	scanf("%d %d %d %d %d", &N, &M, &r, &c, &d);
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			scanf("%d", &G[i][j]);
+		}
+	}
+
+	Clean();
+
+	printf("%d", ans);
+
+	return 0;
+}
