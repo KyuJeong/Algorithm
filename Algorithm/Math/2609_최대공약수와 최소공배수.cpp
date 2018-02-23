16e17749b64a15c4e777a43e18ff5c67bd0de327
KyuJeong <heo0414@naver.com>
2018-02-23 18:22:57 +0900

    두 수의 GCD와 LCM 구하기

---
+++ Algorithm/Math/2609_최대공약수와 최소공배수.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int num1, num2;
+int calGCD(int A, int B)
+{
+	int P;
+	while (A != 0 && B != 0)
+	{
+		if (A >= B)
+		{
+			P = B;
+			A = A % B;
+		}
+		else
+		{
+			P = A;
+			B = B % A;
+		}
+	}
+	return P;
+}
+int GCD;
+int LCM;
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+
+	cin >> num1 >> num2;
+
+	int GCD = calGCD(num1, num2);
+	cout << GCD << "\n";
+	cout << (num1 * num2) / GCD << "\n";
+
+	return 0;
+}
+
