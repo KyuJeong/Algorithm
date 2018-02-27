aa8e6fd3caec81ac2664a53472373a8744e1700e
KyuJeong <heo0414@naver.com>
2018-02-27 22:44:44 +0900

    K개의 수에서 6개를 뽑아서 만들 수 있는 모든 순열

--- Algorithm/Brute-Force/6603_로또.cpp
+++ Algorithm/Brute-Force/6603_로또.cpp
@@ -1,51 +1,46 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
 
-int k = 1;
+int k, S;
 
 int main()
 {
 	std::ios::sync_with_stdio(false);
 
-	while (k != 0)
+	while (cin >> k)
 	{
-		cin >> k;
-
-		vector <int> arr(k);
-
+		vector <int> v(k);
+		vector <int> a;
 		for (int i = 0; i < k; i++)
 		{
-			cin >> arr[i];
+			cin >> v[i];
 		}
-
-		vector <int> v;
 
 		for (int i = 0; i < 6; i++)
 		{
-			v.push_back(1);
+			a.push_back(0);
 		}
 
 		for (int i = 0; i < k - 6; i++)
 		{
-			v.push_back(0);
+			a.push_back(1);
 		}
 
-		do
+		do 
 		{
 			for (int i = 0; i < k; i++)
 			{
-				if (v[i] == 1)
-					cout << arr[i] << " ";
+				if (a[i] == 0)
+					cout << v[i] << " ";
 			}
 			cout << "\n";
-		} while (prev_permutation(v.begin(), v.end()));
+		} while (next_permutation(a.begin(), a.end()));
 
 		cout << "\n";
 	}
-
-
 
 	return 0;
 }
