68f7a2ea79b02c32238acf973eecb0943700a39d
KyuJeong <heo0414@naver.com>
2018-03-06 19:38:19 +0900

    시험장 필요 감독관 수의 최소값

---
+++ Algorithm/SW test/13458_시험 감독.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+int A[1000001];
+int B, C;
+int ans;
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> A[i];
+	}
+
+	cin >> B >> C;
+
+	ans += N;
+
+	for (int i = 0; i < N; i++)
+	{
+		A[i] -= B;
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		if (A[i] > 0)
+			ans += (A[i] - 1) / C + 1;
+	}
+
+	cout << ans;
+
+	return 0;
+}
