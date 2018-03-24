a4e23309844ff05aa9c242be90639ce7f4e145a2
KyuJeong <heo0414@naver.com>
2018-03-24 15:40:23 +0900

    다익스트라 기본 문제

---
+++ Algorithm/Graph/Dijkstra/1916_최소비용 구하기.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+#define INF 1e9
+
+typedef vector <int> vi;
+typedef pair <int, int> pii;
+
+int n, m;
+int src, dst;
+vector <vector <pii>> G;
+int from, go, edge;
+vector <int> dist;
+bool visit[1001];
+
+void Dijkstra()
+{
+	priority_queue <pii> pq;
+
+	pq.push({ 0,src });
+
+	while (!pq.empty())
+	{
+		int cur = pq.top().second;
+		int cost = -pq.top().first;
+		pq.pop();
+
+		if (dist[cur] < cost)
+			continue;
+
+		for (auto i : G[cur])
+		{
+			int nxt = i.second;
+			int ncost = i.first + cost;
+			if (dist[nxt] > ncost)
+			{
+				dist[nxt] = ncost;
+				pq.push({ -ncost,nxt });
+			}
+		}
+
+	}
+	
+}
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+
+	cin >> n >> m;
+
+	G.resize(n + 1);
+	dist.resize(n + 1);
+
+	for (int i = 1; i <= n; i++)
+	{
+		dist[i] = INF;
+	}
+
+	for (int i = 0; i < m; i++)
+	{
+		cin >> from >> go >> edge;
+		G[from].push_back({ edge,go });
+	}
+
+	cin >> src >> dst;
+
+	Dijkstra();
+
+	cout << dist[dst];
+
+	return 0;
+}
