f92044769020d6da98afd0a479f9ec170c363ecd
KyuJeong <heo0414@naver.com>
2018-03-25 16:53:36 +0900

    분할로 제곱수 구하기

---
+++ Algorithm/Divide and Conquer/1009_분산처리.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+typedef long long ll;
+
+int T;
+
+ll Solve(int a, int b)
+{
+	ll ret;
+
+	if (b == 1)
+		return a;
+
+	if (b % 2 == 0)
+	{
+		ll tmp = Solve(a, b / 2);
+		ret = (tmp * tmp) % 10;
+	}
+	else
+	{
+		ll tmp = Solve(a, (b - 1) / 2);
+		ret = (tmp * tmp * a) % 10;
+	}
+
+	return ret;
+}
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+
+	cin >> T;
+
+	while (T--)
+	{
+		int a, b;
+		cin >> a >> b;
+
+		ll ans = Solve(a, b);
+
+		if (ans == 0)
+			cout << 10 << "\n";
+		else
+			cout << ans << "\n";
+	}
+
+	return 0;
+}
