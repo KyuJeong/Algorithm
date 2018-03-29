52adc11f3d71aebb4adfda2e06fda3595127f75d
KyuJeong <heo0414@naver.com>
2018-03-30 02:28:11 +0900

    마이크로소프트 로고 출력

---
+++ Algorithm/Input and Output/5338_마이크로소프트 로고.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	std::ios::sync_with_stdio(false);
+
+	cout << "       _.-;;-._\n";
+	cout << "'-..-'|   ||   |\n";
+	cout << "'-..-'|_.-;;-._|\n";
+	cout << "'-..-'|   ||   |\n";
+	cout << "'-..-'|_.-''-._|\n";
+
+	return 0;
+}
