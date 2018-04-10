48283eaca0fc4e3d2b898455d6fb63bdf96d3b5f
KyuJeong <heo0414@naver.com>
2018-04-10 17:24:07 +0900

    단순 별찍기 ㅁ

---
+++ Algorithm/Input and Output/2556_별찍기 - 14.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int main()
+{
+	cin.tie(NULL);
+	std::ios::sync_with_stdio(false);
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
