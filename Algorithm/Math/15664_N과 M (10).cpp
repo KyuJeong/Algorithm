f591fee0d2707ea7dcf432c64a0140106d091d8a
KyuJeong <heo0414@naver.com>
2018-04-17 19:48:08 +0900

    N개의 수에서 M개 고른 수열(비내림차순, 입력에 중복있음)

---
+++ Algorithm/Math/15664_N과 M (10).cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+typedef vector <int> vi;
+typedef vector <bool> vb;
+
+int N, M;
+vb check;
+vi arr;
+bool repeat_flag[9][10001]; // [N][M] = 카운트가 N일 때 M을 사용했는지 여부
+
+void Solve(int cnt, int cur, vi v)
+{
+	if (cnt > M)
+	{
+		for (int i = 0; i < M; i++)
+		{
+			int idx = v[i];
+			cout << arr[idx] << " ";
+		}
+
+		cout << "\n";
+		return;
+	}
+
+	for (int i = cur; i <= N; i++)
+	{
+		int idx = arr[i];
+		if (!check[i] && !repeat_flag[cnt][idx])
+		{
+			check[i] = true;
+			repeat_flag[cnt][idx] = true;
+			v.push_back(i);
+
+			Solve(cnt + 1, i, v);
+
+			check[i] = false;
+			v.pop_back();
+		}
+	}
+
+	for (int i = 1; i <= N; i++)
+	{
+		int idx = arr[i];
+		repeat_flag[cnt][idx] = false;
+	}
+}
+
+int main()
+{
+	cin.tie(NULL);
+	std::ios::sync_with_stdio(false);
+
+	cin >> N >> M;
+	arr.resize(N + 1);
+
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> arr[i];
+	}
+	sort(arr.begin(), arr.end());
+
+	check.resize(N + 1);
+
+	vi v;
+
+	Solve(1, 1, v);
+
+	return 0;
+}
