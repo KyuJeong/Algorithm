7b480491836e61d2b7f111517e4f4c5a0acc3c8a
KyuJeong <heo0414@naver.com>
2018-04-19 03:39:09 +0900

    두 배열의 합 (투 포인터)

--- Algorithm/Brute-Force/2143_두 배열의 합.cpp
+++ Algorithm/Brute-Force/2143_두 배열의 합.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int T, N, M;
-int A[1001];
-int B[1001];
-int ans;
+int tmp;
+vector <int> A;
+vector <int> B;
+long long ans;
 
 int main()
 {
@@ -16,12 +18,67 @@ int main()
 	cin >> T;
 	
 	cin >> N;
-	for (int i = 0; i < N; i++)
-		cin >> A[i];
-	
+	int idx = 0;
+	for (int i = 0; i < N; i++) // A의 모든 부 배열을 저장
+	{
+		cin >> tmp;
+		A.push_back(tmp);
+		for (int j = 0; j < i; j++)
+		{
+			A.push_back(A[idx++] + tmp);
+		}
+	}
+
 	cin >> M;
+	idx = 0;
 	for (int i = 0; i < M; i++)
-		cin >> B[i];
+	{
+		cin >> tmp;
+		B.push_back(tmp);
+		for (int j = 0; j < i; j++)
+		{
+			B.push_back(B[idx++] + tmp);
+		}
+	}
+
+	sort(A.begin(), A.end());
+	sort(B.begin(), B.end());
+
+	int L = 0;
+	int R = B.size() - 1;
+
+	while (L < A.size() && R >= 0)
+	{
+		int sum = A[L] + B[R];
+		if (sum < T)
+			L++;
+		else if (sum > T)
+			R--;
+		else
+		{
+			int a = A[L];
+			int b = B[R];
+			long long a_cnt = 0;
+			long long b_cnt = 0;
+			while (a == A[L])
+			{
+				a_cnt++;
+				L++;
+				if (L >= A.size())
+					break;
+			}
+			while (b == B[R])
+			{
+				b_cnt++;
+				R--;
+				if (R < 0)
+					break;
+			}
+			ans += a_cnt * b_cnt;
+		}
+	}
+
+	cout << ans;
 
 	return 0;
 }
