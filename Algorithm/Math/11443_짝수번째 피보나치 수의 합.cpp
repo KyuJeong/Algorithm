0ae1e54348080e1243f1b133e02a59be5d0ca29e
KyuJeong <heo0414@naver.com>
2018-04-19 18:33:28 +0900

    짝수번째 피보나치 수들의 합(분할 정복)

---
+++ Algorithm/Math/11443_짝수번째 피보나치 수의 합.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+
+ll n;
+map <ll, ll> m;
+
+ll Fibonacci(ll x)
+{
+	if (x == 0 || x == 1)
+		return x;
+
+	if (m.count(x) > 0)
+		return m[x];
+
+	ll ret;
+
+	if (x % 2 == 1)
+	{
+		ll a = Fibonacci((x / 2) + 1);
+		ll b = Fibonacci(x / 2);
+		m[x] = a*a + b*b;
+		m[x] %= MOD;
+		return m[x];
+	}
+	else
+	{
+		ll a = Fibonacci((x / 2) - 1);
+		ll b = Fibonacci(x / 2);
+		m[x] = (2 * a + b) * b;
+		m[x] %= MOD;
+		return m[x];
+	}
+}
+
+int main()
+{
+	cin.tie(NULL);
+	std::ios::sync_with_stdio(false);
+
+	cin >> n;
+
+	ll ans;
+	if (n % 2 == 0)
+		ans = Fibonacci(n + 1) - 1;
+	else
+		ans = Fibonacci(n) - 1;
+
+	cout << ans;
+
+	return 0;
+}
