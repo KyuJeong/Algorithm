9d5f1428457b4a4da284a8b7ef6d02d46a1a9b17
KyuJeong <heo0414@naver.com>
2018-07-06 05:31:31 +0900

    세그먼트 트리 비재귀 구현

--- Algorithm/Tree/Segment Tree/2042_구간 합 구하기(비재귀).cpp
+++ Algorithm/Tree/Segment Tree/2042_구간 합 구하기(비재귀).cpp
@@ -4,21 +4,22 @@ using namespace std;
 
 typedef long long ll;
 
-#define MAX 1000001
+#define MAX 1100000
 
 ll t[MAX * 2];
 int N, M, K;
+int n;
 
 void build() // build the tree
 {
-	for (int i = N - 1; i > 0; i--)
+	for (int i = n - 1; i > 0; i--)
 		t[i] = t[2 * i] + t[2 * i + 1];
 }
 
 void update(int pos, int val) // set value at position p
 {
 	pos--; // index 1부터 시작
-	for (t[pos += N] = val; pos > 1; pos >>= 1)
+	for (t[pos += n] = val; pos > 1; pos >>= 1)
 		t[pos >> 1] = t[pos] + t[pos ^ 1];
 }
 
@@ -26,7 +27,7 @@ ll sum(int l, int r) // sum on interval [l, r)
 {
 	ll ans = 0;
 	l--; // index 1부터 시작
-	for (l += N, r += N; l < r; l >>= 1, r >>= 1)
+	for (l += n, r += n; l < r; l >>= 1, r >>= 1)
 	{
 		if (l & 1)
 			ans += t[l++];
@@ -42,9 +43,12 @@ int main()
 	std::ios::sync_with_stdio(false);
 
 	cin >> N >> M >> K;
+	n = 1;
+	while (n < N)
+		n *= 2;
 	for (int i = 0; i < N; i++)
-		cin >> t[N + i];
-	
+		cin >> t[n + i];
+
 	build();
 
 	int a, b, c;
