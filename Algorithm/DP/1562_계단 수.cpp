5247a1b7382e2c66b6ef2ce9ba5415038c687ef2
KyuJeong <heo0414@naver.com>
2018-07-09 19:08:21 +0900

    길이가 N인 계단 수 개수 찾기

--- Algorithm/DP/1562_계단 수.cpp
+++ Algorithm/DP/1562_계단 수.cpp
@@ -6,35 +6,40 @@ using namespace std;
 
 typedef long long ll;
 
-ll DP[101][10];
+int N;
+ll dp[101][10][1025];
 
 int main()
 {
 	cin.tie(NULL);
 	std::ios::sync_with_stdio(false);
 
-	int N;
 	cin >> N;
+	for (int i = 0; i <= 9; i++)
+		dp[1][i][(1 << i)] = 1;
 
-	DP[10][0] = DP[10][9] = 1;
-
-	for (int i = 11; i <= N; i++)
+	for (int i = 2; i <= N; i++)
 	{
-		for (int j = 1; j <= 8; j++)
-			DP[i][j] = (DP[i - 1][j - 1] + DP[i - 1][j + 1]) % MOD;
-		DP[i][0] = DP[i - 1][1];
-		DP[i][9] = DP[i - 1][8];
+		for (int j = 0; j <= 9; j++)
+		{
+			for (int k = 0; k <= 1023; k++)
+			{
+				if (j == 0)
+					dp[i][j][k | (1 << j)] += dp[i - 1][j + 1][k];
+				else if (j == 9)
+					dp[i][j][k | (1 << j)] += dp[i - 1][j - 1][k];
+				else
+					dp[i][j][k | (1 << j)] += (dp[i - 1][j - 1][k] + dp[i - 1][j + 1][k]);
+				dp[i][j][k | (1 << j)] %= MOD;
+			}
+		}
 	}
 
 	ll ans = 0;
 
-	for (int i = 10; i <= N; i++)
-	{
-		for (int j = 1; j <= 9; j++)
-		{
-			ans = (ans + DP[i][j]) % MOD;
-		}
-	}
+	for (int j = 1; j <= 9; j++)
+		ans = (ans + dp[N][j][1023]) % MOD;
+
 	cout << ans;
 
 	return 0;
