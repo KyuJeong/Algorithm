69586b1dd08d0ea25bd1de04ce194f559b5f6254
KyuJeong <heo0414@naver.com>
2018-07-09 15:26:41 +0900

    pq 구현

---
+++ Algorithm/Data Structure/1927_최소 힙.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+
+using namespace std;
+
+#define MAX 100001
+
+template <typename T>
+void swp(T& data1, T& data2)
+{
+	T tmp = data1;
+	data1 = data2;
+	data2 = tmp;
+}
+
+template <typename T>
+struct priority_queue
+{
+	T arr[MAX] = { 0 };
+	int idx = 1;
+	bool empty()
+	{
+		return idx == 1;
+	}
+	int size()
+	{
+		return idx;
+	}
+	void push(T data)
+	{
+		arr[idx++] = data;
+		int cur = idx - 1;
+		while (1)
+		{
+			if (cur == 1) break;
+			int par = cur >> 1;
+			if (arr[cur] < arr[par])
+				swp(arr[cur], arr[par]);
+			else break;
+			cur = par;
+		}
+	}
+	void pop()
+	{
+		if (idx == 1) return;
+
+		arr[1] = arr[--idx];
+		int cur = 1;
+		int next;
+		while (cur << 1 < idx)
+		{
+			next = cur;
+			if (arr[next] > arr[cur << 1])
+				next = cur << 1;
+
+			if (cur << 1 | 1 < idx && arr[next] > arr[cur << 1 | 1])
+				next = cur << 1 | 1;
+
+			if (cur == next) break;
+			swp(arr[cur], arr[next]);
+			cur = next;
+		}
+	}
+	T top()
+	{
+		if (idx == 1)
+		{
+			arr[1] = 0;
+		}
+		return arr[1];
+	}
+};
+
+int main()
+{
+	cin.tie(NULL);
+	std::ios::sync_with_stdio(false);
+
+	int N;
+	cin >> N;
+
+	priority_queue <int> pq;
+	int tmp;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> tmp;
+		if (!tmp)
+		{
+			cout << pq.top() << "\n";
+			pq.pop();
+		}
+		else
+			pq.push(tmp);
+	}
+
+	return 0;
+}
