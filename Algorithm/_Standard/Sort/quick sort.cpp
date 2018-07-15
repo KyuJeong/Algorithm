40c38a8f3966bf07a454efeaf2d0a0af485c3d5a
KyuJeong <heo0414@naver.com>
2018-07-15 23:01:25 +0900

    퀵 소트

---
+++ Algorithm/_Standard/Sort/quick sort.cpp
@@ -0,0 +1,26 @@
+int partition(int arr[], int first, int last)
+{
+	int p = first;	int l = first;
+	int r = last;
+	while (l < r)
+	{
+		while (arr[p] >= arr[l] && l < last)
+			l++;
+		while (arr[p] < arr[r])
+			r--;
+		if (l < r)
+			swap(arr[l], arr[r]);
+	}
+	swap(arr[p], arr[r]);
+	return r;
+}
+
+void quickSort(int arr[], int first, int last)
+{
+	if (first < last)
+	{
+		int s = partition(arr, first, last);
+		quickSort(arr, first, s - 1);
+		quickSort(arr, s + 1, last);
+	}
+}
