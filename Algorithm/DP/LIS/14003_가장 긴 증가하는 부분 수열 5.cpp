36ad082f6e0800dfd913ebd1f684e29b13a3cce1
KyuJeong <heo0414@naver.com>
2018-07-27 05:46:40 +0900

    LIS nlogn 구성 원소까지 추적

---
+++ Algorithm/DP/LIS/14003_가장 긴 증가하는 부분 수열 5.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef vector <int> vi;
+
+int p[1000001], arr[1000001];
+
+int main()
+{
+	cin.tie(NULL);
+	std::ios::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+
+	vi v;
+	v.push_back(-1e9);
+
+	int tmp;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> tmp;
+		arr[i] = tmp;
+		if (v.back() < tmp)
+		{
+			v.push_back(tmp);
+			p[i] = v.size() - 1;
+		}
+		else
+		{
+			auto it = lower_bound(v.begin(), v.end(), tmp);
+			p[i] = it - v.begin();
+			*it = tmp;
+		}
+	}
+
+	vi ans;
+	int idx = v.size() - 1;
+	for (int i = n - 1; i >= 0; i--)
+	{
+		if (p[i] == idx)
+		{
+			ans.push_back(arr[i]);
+			idx--;
+		}
+		if (idx == 0) break;
+	}
+	reverse(ans.begin(), ans.end());
+
+	cout << v.size() - 1 << "\n";
+	for (auto i : ans)
+		cout << i << " ";
+
+	return 0;
+}
