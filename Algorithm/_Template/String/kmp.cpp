2c9cabd5b00dc619fa0e2a4c399589b5c3cdec9d
KyuJeong <heo0414@naver.com>
2018-10-15 02:37:29 +0900

    KMP 알고리즘

---
+++ Algorithm/_Template/String/kmp.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef vector <int> vi;
+
+string s, p;
+vi pi, ans;
+
+void getPi()
+{
+	int m = p.size();
+	pi.resize(m);
+	int j = 0;
+	for (int i = 1; i < m; i++)
+	{
+		while (j > 0 && p[i] != p[j])
+			j = pi[j - 1];
+		if (p[i] == p[j])
+			pi[i] = ++j;
+	}
+}
+
+void kmp()
+{
+	getPi();
+	int n = s.size();
+	int m = p.size();
+	int j = 0;
+	for (int i = 0; i < n; i++)
+	{
+		while (j > 0 && s[i] != p[j])
+			j = pi[j - 1];
+		if (s[i] == p[j])
+		{
+			if (j == m - 1) // ¸ÅÄª ¼º°ø
+			{
+				ans.push_back(i - m + 1);
+				j = pi[j];
+			}
+			else j++;
+		}
+	}
+}
+
+int main()
+{
+	cin.tie(NULL);
+	std::ios::sync_with_stdio(false);
+
+	getline(cin, s);
+	getline(cin, p);
+
+	kmp();
+
+	cout << ans.size() << "\n";
+
+	for (auto i : ans)
+		cout << i + 1 << " ";
+
+	return 0;
+}
