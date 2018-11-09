383c2db8a9602df22861199c1e8ef0e880d4e395
KyuJeong <heo0414@naver.com>
2018-11-09 19:54:10 +0900

    0-1 knapsack problem

---
+++ Algorithm/DP/1535_안녕.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+int d[101]; // 체력이 n남았을 때의 최대 기쁨
+int hp[21];
+int glad[21];
+
+int main()
+{
+	cin.tie(NULL);
+	std::ios::sync_with_stdio(false);
+
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+		cin >> hp[i];
+
+	for (int i = 1; i <= n; i++)
+		cin >> glad[i];
+
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j + hp[i] <= 100; j++)
+			d[j] = max(d[j], d[j + hp[i]] + glad[i]);
+
+	cout << d[1];
+
+	return 0;
+}
