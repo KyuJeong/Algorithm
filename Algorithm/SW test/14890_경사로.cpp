03f4719659a1cc4f2baf50e18b29ceb9145c18b0
kyujeong <heo0414@naver.com>
2020-07-24 22:14:32 +0900

    지도에서 각 행,열에 경사로를 적절히 놔서 끝까지 갈 수 있는지

--- Algorithm/SW test/14890_경사로.cpp
+++ Algorithm/SW test/14890_경사로.cpp
@@ -1,161 +1,38 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int N, L;
-int G[100][100];
+int n, k;
 int ans;
+int G[202][101];
 
-int main()
-{
-	scanf("%d %d", &N, &L);
-	
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < N; j++)
-		{
-			scanf("%d", &G[i][j]);
+int main() {
+	cin.tie(NULL);
+	std::ios::sync_with_stdio(false);
+
+	cin >> n >> k;
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			cin >> G[i][j];
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			G[i + n][j] = G[j][i];
+
+	for (int i = 0; i < n * 2; i++) {
+		int cnt = 1;
+		int j;
+		for (j = 0; j < n - 1; j++) {
+			if (G[i][j] == G[i][j + 1]) cnt++;
+			else if (G[i][j] == G[i][j + 1] - 1 && cnt >= k) cnt = 1;
+			else if (G[i][j] == G[i][j + 1] + 1 && cnt >= 0) cnt = 1 - k;
+			else break;
 		}
+		if (j == n - 1 && cnt >= 0) ans++;
 	}
 
-	for (int i = 0; i < N; i++)
-	{
-		int pre = G[i][0];
-		int pre_cnt = 0;
-		int cnt = 0;
-		bool flag = true;
-		for (int j = 0; j < N; j++)
-		{
-			if (pre == G[i][j])
-				cnt++;
-			// 뒤가 더 높은경우
-			else if (pre == G[i][j] - 1)
-			{
-				if (cnt < L)
-				{
-					flag = false;
-					break;
-				}
-				else
-				{
-					cnt = 1;
-					pre = G[i][j];
-				}
-			}
-			// 뒤가 더 낮은경우
-			else if (pre == G[i][j] + 1)
-			{
-				pre = G[i][j];
-				cnt = 0;
-				for (int k = 0; k < L; k++)
-				{
-					// 범위를 넘어서는 경우
-					if (j + k >= N)
-					{
-						flag = false;
-						break;
-					}
-					if (pre == G[i][j + k])
-					{
-						cnt++;
-						if (cnt == L)
-						{
-							cnt = 0;
-							pre = G[i][j + k];
-							j += k;
-							break;
-						}
-					}
-					if (pre != G[i][j + k])
-					{
-						flag = false;
-						break;
-					}
-				}
-				if (!flag)
-					break;
-			}
-			// 높이가 2 이상 차이나는 경우
-			else
-			{
-				flag = false;
-				break;
-			}
-		}
-		if (flag)
-			ans++;
-	}
-
-	for (int i = 0; i < N; i++)
-	{
-		int pre = G[0][i];
-		int pre_cnt = 0;
-		int cnt = 0;
-		bool flag = true;
-		for (int j = 0; j < N; j++)
-		{
-			if (pre == G[j][i])
-				cnt++;
-			// 뒤가 더 높은경우
-			else if (pre == G[j][i] - 1)
-			{
-				if (cnt < L)
-				{
-					flag = false;
-					break;
-				}
-				else
-				{
-					cnt = 1;
-					pre = G[j][i];
-				}
-			}
-			// 뒤가 더 낮은경우
-			else if (pre == G[j][i] + 1)
-			{
-				pre = G[j][i];
-				cnt = 0;
-				for (int k = 0; k < L; k++)
-				{
-					// 범위를 넘어서는 경우
-					if (j + k >= N)
-					{
-						flag = false;
-						break;
-					}
-					
-					if (pre == G[j + k][i])
-					{
-						cnt++;
-						if (cnt == L)
-						{
-							cnt = 0;
-							pre = G[j + k][i];
-							j += k;
-							break;
-						}
-					}
-					if (pre != G[j + k][i])
-					{
-						flag = false;
-						break;
-					}
-				}
-				if (!flag)
-					break;
-			}
-			// 높이가 2 이상 차이나는 경우
-			else
-			{
-				flag = false;
-				break;
-			}
-		}
-		if (flag)
-			ans++;
-	}
-
-	printf("%d\n", ans);
+	cout << ans;
 
 	return 0;
 }
