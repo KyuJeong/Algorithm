5acdf3b66527aaa1e883ac1c740b49833a03ae90
KyuJeong <heo0414@naver.com>
2022-03-01 17:08:12 +0900

    ccw

---
+++ Algorithm/_Template/Geometry/ccw.cpp
@@ -0,0 +1,5 @@
+double ccw(double x1, double y1, double x2, double y2, double x3, double y3) {
+	double ret = x1 * y2 + x2 * y3 + x3 * y1;
+	ret -= y1 * x2 - y2 * x3 - y3 * x1;
+	return abs(ret);
+}
